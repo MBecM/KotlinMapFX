f71ff11293f64874b2bbd792a3af91660e573237
Mateusz Becker <mateuszbecker@gmail.com>
2018-01-22 22:20:59 +0100

    TestMarker - coord corection when marker is resizing.

--- src/main/kotlin/kotlinmapfx/component/TestMarker.kt
+++ src/main/kotlin/kotlinmapfx/component/TestMarker.kt
@@ -13,12 +13,19 @@ class TestMarker(coord: LatLon, text: String) : Button(text), Marker {
 
     init {
         style = "-fx-shape: \"M13.558,0C6.07,0,0,6.07,0,13.558s13.558,22.346,13.558,22.346s13.558-14.858,13.558-22.346S21.045,0,13.558,0zM13.558,20.762c-4.084,0-7.395-3.311-7.395-7.395s3.311-7.395,7.395-7.395c4.084,0,7.396,3.311,7.396,7.395S17.642,20.762,13.558,20.762z\"; \n" +
-                "-fx-pref-height: 48;\n" +
-                "-fx-pref-width: 35; \n" +
+                "-fx-pref-height: 40;\n" +
+                "-fx-pref-width: 28; \n" +
                 "-fx-border-color: black;\n" +
                 "-fx-border-width: 0.5;\n" +
                 "-fx-background-color: red;\n" +
                 "-fx-effect: dropshadow(two-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 1 , 1);"
+
+        widthProperty().addListener { _ ->
+            translateX = translateX - this.width / 2
+        }
+        heightProperty().addListener { _ ->
+            translateY = translateY - this.height
+        }
     }
 
     override val coordinateProperty: SimpleObjectProperty<LatLon> = SimpleObjectProperty(coord)
